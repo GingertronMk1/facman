34bb4265f98791fed0d4ace69db6cc2b57baf5e2
Jack Ellis <jackellis1504@gmail.com>
2023-07-01 20:10:26 +0100

    uhh
    init i guess

---
+++ app/Console/Commands/GenerateBlueprintFilamentResources.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Yaml\Yaml;
+
+class GenerateBlueprintFilamentResources extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-blueprint-filament-resources';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    public function __construct(
+        private Yaml $yaml
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        /**
+         * Gonna blueprint build and migrate,
+         * then handle the filamenting
+         */
+        // $this->call('blueprint:build', ['--overwrite-migrations']);
+        // $this->call('migrate', ['--force']);
+
+
+        $yamlFile = base_path('draft.yaml');
+        $yamlContents = $this->yaml->parseFile($yamlFile);
+        $this->info(json_encode($yamlContents, 128));
+
+        if (!isset($yamlContents['models'])) {
+            return 1;
+        }
+
+        foreach ($yamlContents['models'] as $modelName => $modelProperties) {
+            $args = ['--generate'];
+            if (in_array('softDeletes', $modelProperties)) {
+                $args[] = ['--soft-deletes'];
+            }
+            $this->call('make:filament-resource', $args);
+        }
+
+    }
+}
