571e8c295ad10f8163ec6766f79a3305fa91d259
Jack Ellis <jackellis1504@gmail.com>
2023-12-18 19:48:03 +0000

    Updates

---
+++ src/Infrastructure/Console/MakeEntity.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Console;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'facman:make-entity',
+    description: 'Creates a model, repository, and finder for a given class name',
+    hidden: false,
+)]
+final class MakeEntity extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->addArgument('classname', InputArgument::REQUIRED, 'The name of the model class')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // retrieve the argument value using getArgument()
+        $output->writeln('Username: '.$input->getArgument('classname'));
+        return Command::SUCCESS;
+    }
+    
+}
