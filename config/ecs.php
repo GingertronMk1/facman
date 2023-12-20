c19f54f63116afd74de1e80131aac7c5ef797252
Jack Ellis <jackellis1504@gmail.com>
2023-12-20 22:50:11 +0000

    move

---
+++ config/ecs.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__.'/../assets',
+        __DIR__.'/../config',
+        __DIR__.'/../public',
+        __DIR__.'/../src',
+        __DIR__.'/../tests',
+    ]);
+
+    $ecsConfig->dynamicSets(['@Symfony']);
+};
