40714447edbbb6f938a8a927c7cc6947c40a73ec
Jack Ellis <jackellis1504@gmail.com>
2023-12-20 20:53:58 +0000

    update

--- src/Framework/Console/MakeEntity.php
+++ src/Framework/Console/MakeEntity.php
@@ -8,7 +8,9 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 #[AsCommand(
@@ -42,25 +44,45 @@ final class MakeEntity extends Command
     protected function configure(): void
     {
         $this
-            // ...
             ->addArgument('className', InputArgument::REQUIRED, 'The name of the class')
+            ->addOption(
+                'dry-run',
+                null,
+                InputOption::VALUE_NONE,
+                'Add to not actually write any files'
+            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
         $className = $input->getArgument('className');
+        $dryRun = (boolean) $input->getOption('dry-run');
+        echo ($dryRun ? 'dry run' : 'not dry run');
         foreach(self::PLACES_AND_THINGS as $place => $things) {
             $place = str_replace(self::CLASSNAME_PLACEHOLDER, $className, $place);
             $dirName = $this->kernel->getProjectDir() . "/src/{$place}";
-            if (!is_dir($dirName)) {
-                mkdir($dirName, recursive: true);
+            $io->section($dirName);
+            if (!$dryRun) {
+                if (!is_dir($dirName)) {
+                    $io->text("Making {$dirName}");
+                    mkdir($dirName, recursive: true);
+                } else {
+                    $io->text("{$dirName} already exists");
+                }
+            } else {
+                $io->text("Not making {$dirName}");
             }
             foreach($things as $thing) {
                 $thing = str_replace(self::CLASSNAME_PLACEHOLDER, $className, $thing);
                 $qualifiedFileName = "{$dirName}/{$thing}.php";
-                $output->writeln($qualifiedFileName);
-                touch($qualifiedFileName);
+                if (!$dryRun) {
+                    $io->text("Making {$qualifiedFileName}");
+                    touch($qualifiedFileName);
+                } else {
+                    $io->text("Not actually making {$qualifiedFileName}");
+                }
             }
         }
         return self::SUCCESS;
