1d16bb3fbf49ff5141cfdc4c18b604caff81d397
Jack Ellis <jackellis1504@gmail.com>
2024-07-19 15:15:25 +0100

    wip
     M config/packages/security.yaml
     M src/Application/User/Command/CreateUserCommand.php
     M src/Application/User/Command/UpdateUserCommand.php
    D  src/Framework/Controller/UserController.php
    D  src/Framework/Form/User/CreateUserForm.php
    D  src/Framework/Form/User/UpdateUserForm.php
     M templates/app/framework/cli_command/create_entity_command/application/class_name/command/create_class_name_command.php.twig
    ?? src/Framework/Controller/
    ?? templates/login/

--- src/Application/User/Command/UpdateUserCommand.php
+++ src/Application/User/Command/UpdateUserCommand.php
@@ -1,3 +1,14 @@
 <?php
 
 declare(strict_types=1);
+
+namespace App\Application\User\Command;
+
+use App\Domain\User\ValueObject\UserId;
+
+class UpdateUserCommand {
+    public function __construct(
+        public UserId $id
+    )
+    {}
+}
